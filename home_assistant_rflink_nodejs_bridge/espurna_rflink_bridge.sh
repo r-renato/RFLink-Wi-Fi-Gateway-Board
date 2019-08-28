f345d76f1ce53ef568dd39d7c730436c4aa9d664
Renato Rossi <renatrossi@gmail.com>
2019-08-28 07:27:28 +0200

    Baseline

--- home_assistant_rflink_nodejs_bridge/espurna_rflink_bridge.sh
+++ home_assistant_rflink_nodejs_bridge/espurna_rflink_bridge.sh
@@ -2,12 +2,16 @@
 
 TMS=$(date +"%Y-%m-%d %H-%M-%S")
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-PID=$(ps -ef | grep -E "node|espurna_rflink_bridge.js" | grep -v "grep" | awk '{print $2}')
+PID=$(ps -ef | grep -E "espurna_rflink_bridge.js" | grep -v "grep" | awk '{print $2}')
 
 if [ -z "$PID" ];
 then
+  nohup /usr/bin/node $DIR/espurna_rflink_bridge.js >/dev/null 2>&1 &
+
+  sleep 2
+
+  PID=$(ps -ef | grep -E "espurna_rflink_bridge.js" | grep -v "grep" | awk '{print $2}')
+
   echo "$TMS DIR=$DIR"
   echo "$TMS PID=$PID"
-
-  nohup /usr/bin/node $DIR/espurna_rflink_bridge.js >/dev/null 2>&1 &
 fi
